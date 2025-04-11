bef786296cd51f32cb8f1857a86e91ece014a9cd
Ashish Kushwaha <ashish@wishfy.club>
2025-04-11 21:46:39 +0530

    goroutine waitgroup

--- 9_goroutine/main.go
+++ 9_goroutine/main.go
@@ -2,25 +2,28 @@ package main
 
 import (
 	"fmt"
-	"time"
+	"sync"
 )
 
 // for multithreding
 
-func task(num int) {
+func task(num int, wg *sync.WaitGroup) {
+	defer wg.Done()
 	fmt.Println("doing task: ", num)
 }
 
 func main() {
+	var wg sync.WaitGroup
 	for i := 0; i < 10; i++ {
-		// go task(i)
+		wg.Add(1)
+		go task(i, &wg)
 
 		// good practice to take value as arg
-		go func(num int) {
-			fmt.Println(num)
-		} (i)
+		// go func(num int) {
+		// 	fmt.Println(num)
+		// } (i)
 	}
 
 
-	time.Sleep(2)
+	wg.Wait()
 }
