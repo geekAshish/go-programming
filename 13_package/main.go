36d0a9ebe2ac8a3e1502651784d08a0c6c07b4f9
Ashish Kushwaha <ashish@wishfy.club>
2025-04-12 14:48:42 +0530

    package

--- 13_package/main.go
+++ 13_package/main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/fatih/color"
+	"github.com/geekAshish/go-programming/tree/main/13_package/auth"
+	"github.com/geekAshish/go-programming/tree/main/13_package/user"
+)
+
 func main() {
+	auth.LoginWithCredentials("ashish", "kushwaha")
+	sessionDetail := auth.GetSession()
+	
+	user := user.User{
+		Email: "ashish@cool.com",
+		Username: "kakoii",
+	}
+
+	fmt.Println(sessionDetail, user)
+
+	color.Cyan(user.Username)
+	color.Red(user.Email)
 
 }
