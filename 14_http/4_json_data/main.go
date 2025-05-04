595322862f75126cd05e6f64e8ce29a26d810c60
Ashish Kushwaha <ashish@wishfy.club>
2025-05-04 14:29:51 +0530

    consume json data

--- 14_http/4_json_data/main.go
+++ 14_http/4_json_data/main.go
@@ -37,3 +37,39 @@ func EncodedJson() {
 
 }
 
+func DecodedJson() {
+	jsonDataFromWeb := []byte(`
+	{
+		"name": "ashish",
+		"price": 22,
+		"email": "@mail",
+		"password": "33",
+		tags: ["aa", "ww"]
+	}
+	`)
+
+	var courses course
+
+	checkValid := json.Valid(jsonDataFromWeb)
+
+	if checkValid {
+		fmt.Println("JSON IS VALID")
+		json.Unmarshal(jsonDataFromWeb, &courses)
+		fmt.Printf("%#v\n", courses);
+	} else {
+		fmt.Println("JSON IS NOT VALID")
+	}
+
+
+	// some cases where you just want to add data to key value
+
+
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Printf("%#v\n", myOnlineData);
+
+	for key, value := range myOnlineData {
+		fmt.Printf("key is %v and value is %v and type is %T\n", key, value, value)
+	}
+
+}
