c7afa925026bdd4f9b79ac2ef74d3d6901a7c10b
Ashish Kushwaha <ashish@wishfy.club>
2025-06-15 11:19:19 +0530

    wait group

--- 18_goroutines/main.go
+++ 18_goroutines/main.go
@@ -1,14 +1,42 @@
 package goroutines
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+var wg sync.WaitGroup // usualy these are pointers
 
 func main() {
 	greet("hey")
 	greet("bro")
+
+	websiteList := []string {"github.com", "google.com", "fb.com"};
+
+	for _, web := range websiteList {
+		go getStatusCode(web)
+		wg.Add(1)
+	}
+
+	wg.Wait()
 }
 
 func greet(s string) {
 	for i := 0; i < 5; i++ {
 		fmt.Println(s)
 	}
+}
+
+// wait group
+func getStatusCode(endpoint string) {
+	wg.Done()
+	
+	res, err := http.Get(endpoint)
+
+	if err != nil {
+		fmt.Println("OOPS in endpoint")
+	}
+
+	fmt.Println(res.StatusCode, endpoint)
 }
